6b43a695a20abc6b3a6f110d6882b1909af174cb
Tyrone Singleton <tyronesingleton2003@yahoo.com>
2025-10-11 18:58:18 -0400

    Added final batch of python scripts.

---
+++ KodeKloud/while-conditional3.py
@@ -0,0 +1,5 @@
+x = 0 # assign the x variable a value of 0
+while ( x < 50 ): # starting the loop while the value of x is less than 50 
+    x += 2 # increasing the value of x by adding 2
+    print(x) # printing the value of x inside the loop
+print(x) # printing the value of x the instant it gets over 50
