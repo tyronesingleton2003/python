6902a0f200a4e9b8ac702d1c78ec143cd5c8c267
Tyrone Singleton <tyronesingleton2003@yahoo.com>
2025-10-11 17:47:09 -0400

    Added first batch of git files.

---
+++ KodeKloud/for-quiz2.py
@@ -0,0 +1,4 @@
+x = 'abcd' # Sets the variable for x
+for i in x: # Looping through x, which is represents a, b, c and d
+    print(i) # Prints one letter, a, b c and d, at a time
+    x.upper() # This does nothing
